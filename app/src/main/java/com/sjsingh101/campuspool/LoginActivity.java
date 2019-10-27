a842ef547aaeb9243dd868535dc883313292ca55
Simranjeet Singh <sjsingh9090@gmail.com>
2019-10-27 16:48:39 +0530

    Login And Signup Completed

---
+++ app/src/main/java/com/sjsingh101/campuspool/LoginActivity.java
@@ -0,0 +1,28 @@
+package com.sjsingh101.campuspool;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.material.button.MaterialButton;
+
+public class LoginActivity extends AppCompatActivity {
+
+    TextView btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        btn = findViewById(R.id.link_signup);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this,SignupActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
